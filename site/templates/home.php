ae1684d63ce5df8094eaf845b717c8cbfc4d915c
Tim Ballaschke <tim@art-of-x.com>
2025-10-01 19:54:35 +0200

    chore: initial commit

---
+++ site/templates/home.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Miteinander e.V.</title>
+    <link rel="stylesheet" href="<?= url('assets/style/style.css') ?>?v=3">
+</head>
+<body>
+    <div id="sidebar">
+        <div id="top-menu">
+            <div class="top-menu-element-container">
+                <div class="top-menu-element"></div>
+            </div>
+            <div class="top-menu-element-container">
+                <div class="top-menu-element"></div>
+            </div>
+            <div class="top-menu-element-container">
+                <div class="top-menu-element"></div>
+            </div>
+            <div class="top-menu-element-container">
+                <div class="top-menu-element"></div>
+            </div>
+        </div>
+    </div>
+    <div id="main">
+        <div id="website-title-container">
+            <div id="website-title">Rechtsextremismus in Famlilien<br> und Pädagogik begegnen</div>
+        </div>
+        <div id="content"></div>
+    </div>  
+</body>
+</html>
