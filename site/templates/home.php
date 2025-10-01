bb4f9ea3b1e3406e1c7078a529aaee57a7705ecc
Tim Ballaschke <tim@art-of-x.com>
2025-10-01 20:48:45 +0200

    feat: add Alpine.js for interactivity and update styles for improved layout and top menu

--- site/templates/home.php
+++ site/templates/home.php
@@ -5,21 +5,22 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Miteinander e.V.</title>
     <link rel="stylesheet" href="<?= url('assets/style/style.css') ?>?v=3">
+    <script src="<?= url('assets/js/alpine.min.js') ?>" defer></script>
 </head>
 <body>
     <div id="sidebar">
         <div id="top-menu">
             <div class="top-menu-element-container">
-                <div class="top-menu-element"></div>
+                <div class="top-menu-element">Inhalte</div>
             </div>
             <div class="top-menu-element-container">
-                <div class="top-menu-element"></div>
+                <div class="top-menu-element">Informationen</div>
             </div>
             <div class="top-menu-element-container">
-                <div class="top-menu-element"></div>
+                <div class="top-menu-element">Beratungsangebote</div>
             </div>
             <div class="top-menu-element-container">
-                <div class="top-menu-element"></div>
+                <div class="top-menu-element">Kontakt</div>
             </div>
         </div>
     </div>
