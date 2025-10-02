4e12f5dea96d41365ad5095d113e6a133e284def
Tim Ballaschke <tim@art-of-x.com>
2025-10-02 09:42:10 +0200

    feat: add base styles, typography, and CSS variables; enhance layout with new sidebar options and content display modes

--- site/templates/home.php
+++ site/templates/home.php
@@ -7,7 +7,7 @@
     <link rel="stylesheet" href="<?= url('assets/style/style.css') ?>?v=3">
     <script src="<?= url('assets/js/alpine.min.js') ?>" defer></script>
 </head>
-<body>
+<body x-data="{ view: 'grid' }">
     <div id="sidebar">
         <div id="top-menu">
             <div class="top-menu-element-container">
@@ -23,12 +23,29 @@
                 <div class="top-menu-element">Kontakt</div>
             </div>
         </div>
+        <div class="sidebar-options">
+            <div class="options-container">
+                <div class="options-title">Ansicht:</div>
+                <div id="view-switch" class="options-element">
+                    <button @click="view = 'grid'" :class="{ 'active': view === 'grid' }">Grid</button>
+                    <button @click="view = 'list'" :class="{ 'active': view === 'list' }">List</button>
+                </div>
+            </div>
+        </div>
     </div>
     <div id="main">
         <div id="website-title-container">
             <div id="website-title">Rechtsextremismus in Famlilien<br> und Pädagogik begegnen</div>
         </div>
-        <div id="content"></div>
+        <div id="content" :class="view">
+            <div class="article-container">
+                <div class="article-headline">Extrem rechtes Elternhaus</div>
+                <div class="article-teaser-text">
+                    what if I wanted to make this some kind of component as it gets repeated multiple times. There will be multiple options-container always with a title element and a options element. Would it make sense to use alpine.js for this? Please do not implement any code but explain to me if this would make sense.
+                </div>
+                <div class="article-tags"></div>
+            </div>
+        </div>
     </div>  
 </body>
 </html>
